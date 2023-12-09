b5916eeff969f2ccee116d53c67e31fc7d4a9823
IlhanKurultay <ilhan.kurultay@student.ehb.be>
2023-12-09 17:47:07 +0100

    Enhance 3D Scene with Lighting, Helpers, and Random Stars
    Expand 3D scene functionality by adding a point light source, ambient light, light and grid helpers, and a star field. The new code enhances the visual aesthetics and interactivity, featuring a dynamically lit environment with added spatial references and 200 randomly positioned stars.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import "./style.css";
 
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const scene = new THREE.Scene();
 
@@ -30,8 +31,30 @@ const torus = new THREE.Mesh(geometry, material);
 scene.add(torus);
 
 const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(20, 20, 20);
-scene.add(pointLight);
+pointLight.position.set(0, 0, 0);
+const ambienLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambienLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+Array(200).fill().forEach(addStar);
+
 function animate() {
   requestAnimationFrame(animate);
 
